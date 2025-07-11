2dbc0851e86e0c8902e6f4661fb9b7b56dfdd886
Gonzalo <contact@gonzalosalas.com>
2025-07-11 14:08:37 +0200

    getting ready for Render

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const __dirname = path.dirname(__filename);
 
 // Initialize Express
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // View engine setup
 app.set("view engine", "ejs");
